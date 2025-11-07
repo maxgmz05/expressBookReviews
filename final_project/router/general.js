ef50f3d98ec6bd95ca8943cdc4438397b72095f6
Madhih <madhihraheem05@gmail.com>
2025-11-07 15:03:17 +0530

    used asynchronous routes for some routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,55 +22,94 @@ public_users.post("/register", (req, res) => {
 });
 
 // Task 1: Get all books
-public_users.get('/', function (req, res) {
-  res.send(JSON.stringify(books, null, 4));
+public_users.get('/', async function (req, res) {
+  try {
+    const getBooks = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(books);
+        }, 1000);
+      });
+    };
+    
+    const allBooks = await getBooks();
+    res.json(allBooks);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books" });
+  }
 });
 
 // Task 2: Get book details by ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  
-  if (book) {
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const getBookByISBN = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const book = books[isbn];
+          if (book) {
+            resolve(book);
+          } else {
+            reject("Book not found");
+          }
+        }, 1000);
+      });
+    };
+    
+    const book = await getBookByISBN();
     res.json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
+  } catch (error) {
+    res.status(404).json({ message: error });
   }
 });
   
 // Task 3: Get books by author
-public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
-  const matchingBooks = [];
-  
-  for (let id in books) {
-    if (books[id].author.toLowerCase().includes(author.toLowerCase())) {
-      matchingBooks.push(books[id]);
-    }
-  }
-  
-  if (matchingBooks.length > 0) {
-    res.json(matchingBooks);
-  } else {
-    res.status(404).json({ message: "No books found by this author" });
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+    const getBooksByAuthor = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const matchingBooks = [];
+          for (let id in books) {
+            if (books[id].author.toLowerCase().includes(author.toLowerCase())) {
+              matchingBooks.push(books[id]);
+            }
+          }
+          resolve(matchingBooks);
+        }, 1000);
+      });
+    };
+    
+    const booksByAuthor = await getBooksByAuthor();
+    res.json(booksByAuthor);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books by author" });
   }
 });
 
 // Task 4: Get books by title
-public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
-  const matchingBooks = [];
-  
-  for (let id in books) {
-    if (books[id].title.toLowerCase().includes(title.toLowerCase())) {
-      matchingBooks.push(books[id]);
-    }
-  }
-  
-  if (matchingBooks.length > 0) {
-    res.json(matchingBooks);
-  } else {
-    res.status(404).json({ message: "No books found with this title" });
+public_users.get('/async/title/:title', async function (req, res) {
+  try {
+    const title = req.params.title;
+    const getBooksByTitle = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const matchingBooks = [];
+          for (let id in books) {
+            if (books[id].title.toLowerCase().includes(title.toLowerCase())) {
+              matchingBooks.push(books[id]);
+            }
+          }
+          resolve(matchingBooks);
+        }, 1000);
+      });
+    };
+    
+    const booksByTitle = await getBooksByTitle();
+    res.json(booksByTitle);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books by title" });
   }
 });
 
